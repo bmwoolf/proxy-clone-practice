93bfd6795d4c1dc5fc10d5e52e538b6ceec73937
Bradley Woolf <bradleymwoolf@gmail.com>
2022-01-30 14:13:35 -0800

    proxy pattern

--- proxy.sol
+++ proxy.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/presets/ERC20PresetFixedSupplyUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/";
 
 contract NaiveFactory {
     function createToken(string calldata name, string calldata symbol, uint256 initialSupply) external returns (address) {
@@ -9,8 +10,25 @@ contract NaiveFactory {
         ERC20PresetFixedSupplyUpgradeable token = new ERC20PresetFixedSupplyUpgradeable();
         // create the token from it, passing in the custom parameters
         token.initialize(name, symbol, initialSupply, msg.sender);
-
         // return its new token address
         return address(token);
+    }
+}
+
+
+contract ProxyFactory {
+    address immutable tokenImplementation;
+
+    constructor() public {
+        tokenImplementation = address(new ERC20PresetFixedSupplyUpgradeable());
+    }
+    /// PLUS: we don't need to deploy a new erc20 every time
+    /// MINUS: we need to deploy a new proxy every time- which is cheaper than Naive Factory, but still more than a clone
+    function createToken(string calldata name, string calldata symbol, uint256 initialSupply) external returns (address) {
+        UpgradeableProxy proxy = new UpgradeableProxy(
+            tokenImplementation,
+            abe.encodeWithSelector(ERC20PresetFixedSupplyUpgradeable(0).initialize.selector, name, symbol, initialSupply, msg.sender)
+        );
+
     }
 }
